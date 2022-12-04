2969d202b1e79862ce67054f27390dc76ef875f8
Jesse Rusak <me@jesserusak.com>
2022-12-04 11:50:24 -0500

    day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,17 @@
+#[allow(unused_imports)]
 use aoc2022::*;
+use itertools::Itertools;
+use std::fs;
 
 fn main() {
-    add(1, 2);
-    println!("hi")
+    let max: u32 = fs::read_to_string("input/day1.txt")
+        .expect("Failed to read input file")
+        .lines()
+        .map(|a| a.parse().unwrap_or_default())
+        .coalesce(|a: u32, b| if b == 0 { Err((a, b)) } else { Ok(a + b) })
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<u32>();
+    println!("Max: {:?}", max);
 }
