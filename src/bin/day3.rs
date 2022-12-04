b5b9ce45d689f3fc3d1a4e3ae8369f5188543359
Jesse Rusak <me@jesserusak.com>
2022-12-04 13:22:10 -0500

    day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,19 +1,36 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
 use aoc2022::*;
 use itertools::Itertools;
 use std::{collections::HashSet, fs};
 
 fn main() {
-    let result: u32 = fs::read_to_string("input/day3.txt")
+    let result: usize = fs::read_to_string("input/day3.txt")
         .expect("Failed to read input file")
         .lines()
-        .map(score_rucksack)
+        .chunks(3)
+        .into_iter()
+        .map(find_common)
         .sum();
 
     println!("Result: {:?}", result);
 }
 
-fn score_rucksack(line: &str) -> u32 {
+fn find_common<'a>(i: impl Iterator<Item = &'a str>) -> usize {
+    let mut counts = [0usize; 26 * 2 + 1];
+    for line in i {
+        for c in line.chars().unique() {
+            counts[score_for(c)] += 1;
+        }
+    }
+
+    counts
+        .iter()
+        .enumerate()
+        .find_map(|(i, c)| if *c == 3 { Some(i) } else { None })
+        .unwrap()
+}
+
+fn score_rucksack(line: &str) -> usize {
     let (items_first, items_second) = line.split_at(line.len() / 2);
     let items_first: HashSet<char> = items_first.chars().collect();
     let common = items_second
@@ -23,10 +40,10 @@ fn score_rucksack(line: &str) -> u32 {
     score_for(common)
 }
 
-fn score_for(c: char) -> u32 {
+fn score_for(c: char) -> usize {
     match c {
-        'a'..='z' => (c as u32) - ('a' as u32) + 1,
-        'A'..='Z' => (c as u32) - ('A' as u32) + 27,
+        'a'..='z' => (c as usize) - ('a' as usize) + 1,
+        'A'..='Z' => (c as usize) - ('A' as usize) + 27,
         _ => panic!("Invalid character: {}", c),
     }
 }
